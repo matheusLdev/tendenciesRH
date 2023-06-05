faa0aba888b7f524af3b1b45bac0a362e201113d
Matheus Gonçalves <goncalves.mld@hotmail.com>
2023-06-04 23:25:38 -0300

    bug:correção menu mobile

--- js/mobile-header.js
+++ js/mobile-header.js
@@ -2,25 +2,25 @@ const btnMobile = document.getElementById("menu-mobile");
 const menu = document.getElementById("menu-header");
 
 function handleMenu() {
-  menu.classList.toggle('active');
-  const active = menu.classList.contains('active');
+  menu.classList.toggle("active");
+  const active = menu.classList.contains("active");
   btnMobile.ariaExpanded = active;
   if(active) {
     btnMobile.ariaLabel = "Fechar Menu";
-    btnMobile.style.backgroundImage = "url('../img/icons/menu-close.svg')";
+    btnMobile.style.backgroundImage = "url(../img/icons/menu-close.svg)";
     btnMobile.style.transition = "0.3s";
   } else {
     btnMobile.ariaLabel = "Abrir Menu";
-    btnMobile.style.backgroundImage = "url('../img/icons/menu-open.svg')";
+    btnMobile.style.backgroundImage = "url(../img/icons/menu-open.svg)";
     btnMobile.style.transition = "0.3s";
   }
 }
 btnMobile.addEventListener("click", handleMenu);
 menu.addEventListener("click", handleMenu);
 
-const header = document.querySelector('header')
+const header = document.querySelector("header")
 
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
   if(this.scrollY > 50){
     header.classList.add("fixed-header")
   } else {
